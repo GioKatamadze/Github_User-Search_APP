b7e745d543c732397aadaf9a51e4d0601cf9c53a
Gio Katamadze <giokat.inbox@gmail.com>
2022-11-29 22:56:36 +0400

    #3 Create APP header – title and color changer.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import './reset.css';
-import './App.css';
-import themeIcon from './assets/icon-sun.svg'
-
+import Header from "./components/header/Header.js";
+import "./reset.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-      <div className='wrapper' >
-
-        <div className='header' >
-          <h1>devfinder</h1>
-          <div className='colorChanger' >
-            <p>LIGHT</p>
-            <div className='colorChangerIcon' > <img src={themeIcon} alt="sun icon" /> </div>
-          </div>
-        </div>
-        
-      </div>
-      
-    </div>
-  );
+  return <div className="App">{Header()}</div>;
 }
 
 export default App;
